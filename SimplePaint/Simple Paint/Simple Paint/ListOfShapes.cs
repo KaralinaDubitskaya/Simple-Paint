1297b0102e26b1f95783a38481266d16e2c35aca
Karolina Dubitskaya <Karolina.D100899@gmail.com>
2018-03-10 20:01:59 +0300

    Add class ListOfShapes

---
+++ SimplePaint/Simple Paint/Simple Paint/ListOfShapes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Simple_Paint
+{
+    public class ListOfShapes
+    {
+        private List<Shape> listOfShapes;
+
+        public ListOfShapes()
+        {
+            listOfShapes = new List<Shape>();
+        }
+
+        public void Add(Shape shape)
+        {
+            listOfShapes.Add(shape);
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            foreach (Shape shape in listOfShapes)
+            {
+                shape.Draw(graphics);
+            }
+        }
+    }
+}
